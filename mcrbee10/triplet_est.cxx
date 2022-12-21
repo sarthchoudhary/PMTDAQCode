c9f3bf0d132e80c8d9323a3a992d72d1d86b36da
Sarthak Choudhary <sarth8d314@gmail.com>
2022-12-21 17:12:40 +0100

    saves plot as pdf

--- mcrbee10/triplet_est.cxx
+++ mcrbee10/triplet_est.cxx
@@ -16,18 +16,28 @@
 #include "TMath.h"
 
 // using namespace std;
+
+void saveplot(TH1* hist_obj)
+{
+	TCanvas* C1 = new TCanvas();
+	hist_obj->Draw();
+	C1->SetLogy();
+	C1->SaveAs("summed_waveform_fit.pdf");
+}
+
 int main(int argc, char** argv) 
 {
-	// TFile* in_file = TFile::Open("/home/sarthak/Documents/Out-R6_Module_0_3.root");
 	TFile* in_file = TFile::Open(argv[1]);
 
 	TH1D* in_hist = (TH1D*)in_file->Get("Stackwaveform_CH1"); // Histogram name needs to change
 
-	// TF1* fit_function = new TF1("fit_function", "[0]*exp([1]*(x+[2]))", atof(argv[2]), atof(argv[3]));
 	TF1* fit_function = new TF1("fit_function", "([0]/[1])*exp(-(x-[2])/[1])", atof(argv[2]), atof(argv[3]));
 	fit_function->SetParameters(1000, 1.0, 0.5);
 	TFitResultPtr fit_result = in_hist->Fit("fit_function", "SR");
-	// Double_t fit_function->GetChisquare();
 	cout <<  "Chi2: " << '\n' << fit_function->GetChisquare() << endl;
+	
+	// saving fit plot as pdf
+	saveplot(in_hist);
+	
 	return 0;
 }
